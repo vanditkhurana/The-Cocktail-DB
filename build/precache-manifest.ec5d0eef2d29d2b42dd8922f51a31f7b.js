0c832f6ce425ec45e416ff49f75fd3e3a8f3b2e2
Vandit Khurana <vanditkhurana34@gmail.com>
2022-04-24 23:29:59 +0530

    Adding build folder

---
+++ build/precache-manifest.ec5d0eef2d29d2b42dd8922f51a31f7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912e85f6bae92b4f018328b49f154aa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac10bfe1a05b88013ebf",
+    "url": "/static/css/main.0220429b.chunk.css"
+  },
+  {
+    "revision": "86f01e628480fad0fa08",
+    "url": "/static/js/2.cf98b4f9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.cf98b4f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac10bfe1a05b88013ebf",
+    "url": "/static/js/main.43d303f2.chunk.js"
+  },
+  {
+    "revision": "bddc15a9d8daafb3da06",
+    "url": "/static/js/runtime-main.44c174db.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
